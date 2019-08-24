67e25a091deeb8ccc4c975ac31012a9b60dab548
Taiwo Sokunbi <sokunbitaiwo82@gmail.com>
2019-08-24 14:56:49 +0100

    add solution to quiz add answers update entity diagram update readme

---
+++ quiz.py
@@ -0,0 +1,19 @@
+ascii_code = [ 104, 116, 116, 112, 115, 58, 47, 47, 101, 110, 103, 105, 110, 101, 101, 114, 105, 110, 103, 45, 97, 112, 112, 108, 105, 99, 97, 116, 105, 111, 110, 46, 98, 114, 105, 116, 101, 99, 111, 114, 101, 46, 99, 111, 109, 47, 113, 117, 105, 122, 47, 119, 101, 102, 102, 107, 102, 112, 102, 108, 101, 109, 115, 105, 115, 111, 100, 100 ]
+
+result = [chr(code) for code in ascii_code ]
+print(''.join(result))
+
+from cryptography.fernet import Fernet
+
+key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
+
+# Oh no! The code is going over the edge! What are you going to do?
+message = b'gAAAAABdWO6DOYTyFtbevIUZAGR2T4_h_8I2XDzjgmIdiCPIfYL5u6meD5DA1EmTMvU8inoJKMqXq5gR6IG0F7nG1ldH432XhhlvzpeHXs-y_18WKhN_2AH3QT2PXGlQqv8LJ7PM2y9RagGEWAaLzTeYHTMxmuGTC5sjgYa5fMqqE6CQ_xuL4zI='
+
+def main():
+    f = Fernet(key)
+    print(f.decrypt(message))
+
+
+if __name__ == "__main__":
+    main()
